9ba0dc83a52bad7c9ed2b14df65a5af234550c20
MaharaniJ <maharani.jeyaraman@outlook.com>
2024-02-04 17:05:03 +0530

    add margin

--- src/comonents/Weathercard.jsx
+++ src/comonents/Weathercard.jsx
@@ -49,7 +49,7 @@ const CountryWeatherInfo = () => {
       {error && <p className="alert alert-danger">{error}</p>}
       {data && (
         <div
-          className="card bg-info shadow-md mx-auto mt-5"
+          className="card bg-info shadow-md mx-auto mt-5 mb-5 "
           style={{
             maxWidth: "20rem",
             width: "100%",
